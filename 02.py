4069edbe792c62b735bd818b1ccf7a1df7b56829
francesco.piccolo <francesco.piccolo@lynxspa.com>
2019-12-04 18:00:53 +0100

    Day 02

--- 02.py
+++ 02.py
@@ -7,16 +7,18 @@ def main():
     input_filename="02input.txt"
     with open(input_filename, 'r') as f:
         x = f.readlines()
-    i=0;
+    i = 0
+    #esempio:
+    x=[1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]
     #finchè opcode non è 99:
-    while x[i]!=99:
-        if x[i]==1:
-            #somma:
-        if x[i]==2:
-            #moltiplica
-        i += 1
-
-
+    while x[i] != 99:
+        if x[i] == 1:
+            x[i+3] = x[i+1]+x[i+2]
+        if x[i] == 2:
+            x[i+3] = x[i+1]*x[i+2]
+        i += 4
+    print (x)
+    #dovrebbe essere 3500,9,10,70,  2,3,11,0,  99 ,30,40,50
 if __name__ == "__main__":
     main()
 
